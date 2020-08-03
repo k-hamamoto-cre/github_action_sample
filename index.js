9a9e1929a605cb9c3fddb41da1b6f04c6a925d09
AITC 濵元 邦明 <k-hamamoto@cresco.co.jp>
2020-08-03 17:04:17 +0900

    setFailedするとActions側でエラー検知する

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello!!! ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  throw new Error("あいうえお");
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello!!! ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    //   const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //   console.log(`The event payload: ${payload}`);
+    throw new Error("あいうえお");
 } catch (error) {
-//   core.setFailed(error.message);
-  console.error("error from console.error(): " + error.message)
+    // setFailedするとActions側でエラー検知する
+    core.setFailed(error.message);
+    console.error("error from console.error(): " + error.message)
 }
